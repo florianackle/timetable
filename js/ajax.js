685e53c53e015835ccb78486cdb1322d799af7e9
florianackle <florianackle@gmail.com>
2018-12-03 10:46:29 +0100

    bug 'only 1 class per job' & 'no class for this job' fixed

--- js/ajax.js
+++ js/ajax.js
@@ -10,35 +10,52 @@ $.getJSON("http://sandbox.gibm.ch/berufe.php", function(data) {
 });
 getClass("");
 
+//Falls sich etwas ändert im dropdown "beruf-dropdown"
+//Ein Beruf wurde gewählt
 $(document).on('change', '#beruf-dropdown', function() {
+    //Tafel und Klassenauswahl leeren
+    $('.calendarrow').html("");
     $('#klasse-dropdown').html("");
-    console.log($('#beruf-dropdown').val());
     var bid = "";
-
+    //Setzen des Strings (nach http://sandbox.gibm.ch/klassen.php)
     if ($('#beruf-dropdown').val() != -1) {
         bid = "?beruf_id=" + $('#beruf-dropdown').val();
     }
+    //führe getClass mit dem Parameter bid aus
     getClass(bid);
 });
-
+//Falls sich etwas ändert im dropdown "klasse-dropdown"
+//Eine Klasse wurde gewählt
 $(document).on('change', '#klasse-dropdown', function() {
-    $('.calendarrow').html("");
-    var kid = "";
-    if ($('#klasse-dropdown').val() != -5) {
-        kid = "?klasse_id=" + $('#klasse-dropdown').val() + "&woche=" + today;
-    }
-    getTafel(kid);
+    fillTafel();
 })
-
+//Zeigt alle Klassen an
+//Falls nur eine Klasse vorhanden ist, wähle diese aus
 function getClass(bid) {
+    //getJSON Abfrage
     $.getJSON("http://sandbox.gibm.ch/klassen.php" + bid, function(data) {
+        //Falls mehr als eine Klasse zurück kommt
+        if (data.length > 1) {
+            //Setze "Wählen Sie eine Klasse" erneut
+            //Für manuelles wählen der Klasse
+            $('#klasse-dropdown').append("<option value='-5'> Wählen Sie eine Klasse </option>");
+        } else if (data.length == 0) { //Falls es keine Klassen gibt
+            //Leeren der vorherigen Tabelle
+            $('.calendarrow').html("");
+            $('.calendarrow').append("<tr><td colspan='5'>Für diesen Beruf gibt es zurzeit keine Klasse.</td></tr>");
+        }
         var items = [];
         $.each(data, function(key, val) {
             $('#klasse-dropdown').append("<option value='" + val.klasse_id + "'>" + val.klasse_longname + "</option>");
         });
+        if (data.length == 1) {
+            fillTafel();
+        }
     });
 }
 
+//Wenn eine Klasse ausgewählt wurde
+//Einfüllen der Tafel in die Tabelle
 function getTafel(kid) {
     $.getJSON("http://sandbox.gibm.ch/tafel.php/" + kid, function(data) {
         var items = [];
@@ -46,4 +63,14 @@ function getTafel(kid) {
             $('.calendarrow').append("<tr><th>" + val.tafel_von + "</th>" + "<th>" + val.tafel_bis + "</th>" + "<th>" + val.tafel_longfach + "</th>" + "<th>" + val.tafel_lehrer + "</th>" + "<th>" + val.tafel_raum + "</th>");
         });
     });
+}
+//Falls es nur eine Klasse gibt für den Beruf
+//durch "getTafel(kid)" Einfüllen der Tafel in die Tabelle
+function fillTafel() {
+    $('.calendarrow').html("");
+    var kid = "";
+    if ($('#klasse-dropdown').val() != -5) {
+        kid = "?klasse_id=" + $('#klasse-dropdown').val() + "&woche=" + today;
+    }
+    getTafel(kid);
 }
--- js/time.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-//
-// $(document).on('change', '#klasse-dropdown', function() {
-//
-// }
